dc10245206dd3728add775a37a049d4308ad7910
ajay <ajaysunarthi@iitj.ac.in>
2016-04-03 17:36:26 +0530

    possible prototype

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 angular.module('gameApp', [])
-  .controller('ctrl', control);
+    .controller('ctrl', control);
 
-function control() {
- 
+control.$inject = ['$scope'];
+
+function control($scope) {
+    var vm = this;
+    vm.grid = new Array();
+
+    vm.firstLevel = function(argument) {
+
+            vm.row = 4;
+            vm.col = 4;
+
+            for (var i = 0; i < vm.row; i++) {
+                vm.grid[i] = new Array();
+                for (var j = 0; j < vm.col; j++) {
+                    var isMarked = Math.random() >= 0.6;
+                    vm.grid[i][j] = {
+                        "isMarked":isMarked
+                    };
+
+                }
+
+            }
+};
+
+vm.firstLevel();
+
 }
